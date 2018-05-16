8e0b27b706825e8ab0283f0f488f1ed6edf1c6ab
Dima Tatarinov <frickieflop@gmail.com>
2018-05-16 23:14:50 +0300

    Правка кода после переноса текстов

--- add_texts.py
+++ add_texts.py
@@ -9,7 +9,7 @@ print('...DataFrame created...')
 
 for i in range(1, 585):
     try:
-        file_name = 'Архив ВШЭ-ФЭ-69-14-2017-' + str(i) + '-utf-8.txt'
+        file_name = 'texts_UTF8/Архив ВШЭ-ФЭ-69-14-2017-' + str(i) + '-utf-8.txt'
         text_name = 'Архив ВШЭ-ФЭ-69-14-2017-' + str(i)
         data = TextData[TextData['Имя файла'] == text_name].values.tolist()
         with open(str(file_name), 'r', encoding='utf-8') as file:
--- analyzer.py
+++ analyzer.py
@@ -12,7 +12,7 @@ def main():
         try:
             line_num = 1
             text_num = i
-            file = open('Архив ВШЭ-ФЭ-69-14-2017-' + str(i) + '-utf-8.txt', 'r',
+            file = open('texts_UTF8/Архив ВШЭ-ФЭ-69-14-2017-' + str(i) + '-utf-8.txt', 'r',
                         encoding='utf-8')
             print('... TEXT ' + str(i) + ' OPENED...')
             lines = re.sub('[А-Я][А-Я]?.+[1-9]?: ', '', file.read()).split('\n')
