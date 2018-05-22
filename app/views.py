ecccbe1acb42f2d6746d89adcbbe1ad13f8c6f2f
Dima Tatarinov <frickieflop@gmail.com>
2018-05-22 17:22:05 +0300

    Главная страница
    Дизигн

--- app/views.py
+++ app/views.py
@@ -13,21 +13,14 @@ def index():
 
 @app.route('/form')
 def form():
-    word = request.args['word']
     lex = [request.args['lex1'], request.args['lex2']]
     gr = [request.args['gr1'], request.args['gr2']]
     num = request.args['num']
-    if word != '':
-        output = setup.setup_line(search.get_lines_2(search.search_word(word)))
-        return render_template('result.html', result=output, word_flag=True)
-    elif len(lex) != 0 or len(gr) != 0:
+    if len(lex) != 0 or len(gr) != 0:
         if lex[1] == '' and gr[1] == '':
-            output = search.get_lines(search.lexgram_search(gr[0], lex[0]))
             output2 = setup.setup_line(search.get_lines_2(search.lexgram_search(gr[0], lex[0])))
             return render_template('result.html', result=output2, word_flag=True)
         else:
-            output = search.get_lines(search.func_name(search.lexgram_search(gr[0], lex[0]),
-                                                       search.lexgram_search(gr[1], lex[1]), int(num)))
             output2 = setup.setup_line(search.get_lines_2(search.func_name(search.lexgram_search(gr[0], lex[0]),
                                                        search.lexgram_search(gr[1], lex[1]), int(num))))
             return render_template('result.html', result=output2, word_flag=True)
@@ -36,5 +29,13 @@ def form():
 @app.route('/text')
 def text():
     ids = request.args['text']
-    text = search.get_text(ids)
+    text = search.get_text(ids.lower())
     return render_template('text.html', result=text)
+
+
+@app.route('/form_word')
+def form_word():
+    word = request.args['word']
+    if word != '':
+        output = setup.setup_line(search.get_lines_2(search.search_word(word)))
+        return render_template('result.html', result=output, word_flag=True)
